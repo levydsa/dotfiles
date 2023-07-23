077c1f7cecb7d3dcefce7feb264a457576b352dd
Levy Albuquerque <levyddsa@gmail.com>
2023-07-23 03:32:46 -0300

    major changes to neovim configuration
     - call packer from init.lua
     - add some explanatory comments to options (adding more in the future)
     - and remove options that don't change behavior
     - add indentation rules for more languages in use (i'll find a better
       solution, the current one is too clunky)

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -14,9 +14,46 @@
 autocmd = vim.api.nvim_create_autocmd
 fn = vim.fn
 
-vim.opt.hidden      = true
-vim.opt.nu          = true
-vim.opt.rnu         = true
+local ensure_packer = function()
+  local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+  if fn.empty(fn.glob(install_path)) > 0 then
+    fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+    vim.cmd [[packadd packer.nvim]]
+    return true
+  end
+  return false
+end
+
+local packer_bootstrap = ensure_packer()
+local packer = require('packer')
+
+packer.startup(function(use)
+  -- Packer
+  use 'wbthomason/packer.nvim'
+
+  -- Julia support
+  use 'JuliaEditorSupport/julia-vim'
+
+  -- Fuzzy OwO
+  use {
+    'nvim-telescope/telescope.nvim', tag = '0.1.x',
+    requires = { {'nvim-lua/plenary.nvim'} }
+  }
+
+  -- General language support
+  use { 'nvim-treesitter/nvim-treesitter', run = ':TSUpdate' }
+
+  -- Gruvbox
+  use 'morhetz/gruvbox'
+
+  if packer_bootstrap then
+    packer.sync()
+    packer.install()
+  end
+end)
+
+vim.opt.nu          = true              -- Line numbers
+vim.opt.rnu         = true              -- Show line number relative to cursor line
 vim.opt.redrawtime  = 10000             -- More time to redraw (Better for larger files)
 vim.opt.cmdheight   = 2                 -- Larger command line
 vim.opt.wrap        = false             -- No warping
@@ -29,10 +66,15 @@ vim.opt.signcolumn  = 'auto'            -- Automatic signs
 vim.opt.colorcolumn = {81}              -- Color Column
 vim.opt.encoding    = 'utf-8'           -- UTF-8 Encoding
 vim.opt.spelllang   = {'en_us','pt_br'} -- Spell check English and Brazilian Portuguese
-vim.opt.showtabline = 2
-vim.opt.shell       = 'bash'
+vim.opt.showtabline = 2                 -- Always show the tab line
+
+-- Show tabs and trailing spaces
 vim.opt.list        = true
 vim.opt.listchars   = { tab = '│ ' , trail = '~' }
+
+vim.opt.autochdir   = true
+vim.opt.mouse       = ""
+vim.opt.formatoptions:remove({'r', 'o'})
 
 -- Who needs airline?
 function status()
@@ -58,7 +100,6 @@ vim.g.mapleader = ' '
 vim.g.c_syntax_for_h = true
 
 vim.g.gruvbox_contrast_dark = 'hard'
-
 vim.cmd.colorscheme('gruvbox')
 
 local typecmd = {
@@ -69,7 +110,9 @@ local typecmd = {
   vim  = [[ setlocal ts=2 ]],
   sh   = [[ setlocal ts=2 ]],
   asm  = [[ setlocal ts=2 ]],
+  cpp  = [[ setlocal ts=4 ]],
   lua  = [[ setlocal ts=2 et ]],
+  elm  = [[ setlocal ts=2 et ]],
   javascript = [[ setlocal ts=4 et ]],
   markdown = [[ setlocal ts=4 et ]],
 }
@@ -86,28 +129,25 @@ autocmd({"TextChanged", "InsertLeave"}, {
   end,
 })
 
-vim.keymap.set('n', '<leader>e', function()
-  vim.cmd [[Fern %:h -drawer -toggle]]
-end)
-
-vim.keymap.set('n', '<leader>s', function()
-  vim.cmd [[setlocal spell!]]
-end)
+local builtin = require('telescope.builtin')
+vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
+vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
+vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
 
 vim.keymap.set('n', '<F3>', function()
   fn.setreg('/', (fn.getreg('/') == '') and fn.expand("<cword>") or '')
 end)
 
-require('plugins')
 
 require('nvim-treesitter.configs').setup {
-  ensure_installed = { "c", "lua", "vim", "python", "go" },
+  ensure_installed = { "c", "cpp", "lua", "vim", "python", "go" },
   sync_install = false,
   auto_install = false,
   highlight = {
     enable = true,
     disable = function(lang, buf)
-      local max_filesize = 100 * 1024 -- 100 KB
+      local max_filesize = 100 * 1024 -- 100 KiB
       local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
       if ok and stats and stats.size > max_filesize then
         return true
