24eb581df10787485a2449e07c223312dfb2513c
Levy Albuquerque <levyddsa@gmail.com>
2023-07-23 03:32:46 -0300

    actually does something now

--- .config/python/startup.py
+++ .config/python/startup.py
@@ -1,14 +1,3 @@
 
-if 'PYTHONHISTFILE' in os.environ:
-    history = os.path.expanduser(os.environ['PYTHONHISTFILE'])
-elif 'XDG_DATA_HOME' in os.environ:
-    history = os.path.join(os.path.expanduser(os.environ['XDG_DATA_HOME']),
-                           'python', 'python_history')
-else:
-    history = os.path.join(os.path.expanduser('~'),
-                           '.python_history')
-
-history = os.path.abspath(history)
-_dir, _ = os.path.split(history)
-os.makedirs(_dir, exist_ok=True)
-
+import readline
+readline.write_history_file = lambda *args: None
