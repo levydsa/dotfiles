c8a8f71c228e13f1312a34fdf03f78b833dc5d08
Levy Albuquerque <levyddsa@gmail.com>
2023-08-27 17:31:50 -0300

    bulk neovim config changes

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,14 +1,7 @@
 
 --
---   .__       .__  __         .__
---   |__| ____ |__|/  |_ ___  _|__| _____
---   |  |/    \|  \   __\\  \/ /  |/     \
---   |  |   |  \  ||  |   \   /|  |  Y Y  \
---   |__|___|  /__||__| /\ \_/ |__|__|_|  /
---           \/         \/              \/
---
---   Minimalist (N)Vim Configuration.
---   By YV31
+--   minimalist neovim configuration
+--   by Levy A.
 --
 
 autocmd = vim.api.nvim_create_autocmd
@@ -28,23 +21,21 @@ local packer_bootstrap = ensure_packer()
 local packer = require('packer')
 
 packer.startup(function(use)
-  -- Packer
+  -- packer
   use 'wbthomason/packer.nvim'
 
-  -- Julia support
-  use 'JuliaEditorSupport/julia-vim'
-
-  -- Fuzzy OwO
+  -- fuzzy OwO
   use {
     'nvim-telescope/telescope.nvim', tag = '0.1.x',
     requires = { {'nvim-lua/plenary.nvim'} }
   }
 
-  -- General language support
+  -- general language support
   use { 'nvim-treesitter/nvim-treesitter', run = ':TSUpdate' }
+  use 'simplosophy/vim-io'
 
-  -- Gruvbox
-  use 'morhetz/gruvbox'
+  -- color scheme
+  use { "bluz71/vim-moonfly-colors", as = "moonfly" }
 
   if packer_bootstrap then
     packer.sync()
@@ -52,31 +43,33 @@ packer.startup(function(use)
   end
 end)
 
-vim.opt.nu          = true              -- Line numbers
-vim.opt.rnu         = true              -- Show line number relative to cursor line
-vim.opt.redrawtime  = 10000             -- More time to redraw (Better for larger files)
-vim.opt.cmdheight   = 2                 -- Larger command line
-vim.opt.wrap        = false             -- No warping
-vim.opt.wildmenu    = true              -- Better completion menu
-vim.opt.ignorecase  = true              -- Ignore case sensitive search
-vim.opt.smartcase   = true              -- Overwrite 'ignorecase' if search has upper case chars
-vim.opt.hlsearch    = true              -- Highlight search
-vim.opt.updatetime  = 300               -- Swap write to disk delay
-vim.opt.signcolumn  = 'auto'            -- Automatic signs
-vim.opt.colorcolumn = {81}              -- Color Column
-vim.opt.encoding    = 'utf-8'           -- UTF-8 Encoding
-vim.opt.spelllang   = {'en_us','pt_br'} -- Spell check English and Brazilian Portuguese
-vim.opt.showtabline = 2                 -- Always show the tab line
+vim.opt.nu = true                     -- line numbers
+vim.opt.rnu = true                    -- show line number relative to cursor line
+vim.opt.redrawtime = 10000            -- more time to redraw (better for larger files)
+vim.opt.cmdheight = 2                 -- larger command line
+vim.opt.wrap = false                  -- no warping
+vim.opt.wildmenu = true               -- better completion menu
+vim.opt.ignorecase = true             -- ignore case sensitive search
+vim.opt.smartcase = true              -- overwrite 'ignorecase' if search has upper case chars
+vim.opt.hlsearch = true               -- highlight search
+vim.opt.updatetime = 300              -- swap write to disk delay
+vim.opt.signcolumn = 'auto'           -- automatic signs
+vim.opt.colorcolumn = {81}            -- color column
+vim.opt.encoding = 'utf-8'            -- utf-8 encoding
+vim.opt.spelllang = {'en_us','pt_br'} -- spell check English and Brazilian Portuguese
+vim.opt.showtabline = 2               -- always show the tab line
 
 -- Show tabs and trailing spaces
-vim.opt.list        = true
-vim.opt.listchars   = { tab = '│ ' , trail = '~' }
+vim.opt.list = true
+vim.opt.listchars = { tab = '│ ' , trail = '~' }
 
-vim.opt.autochdir   = true
-vim.opt.mouse       = ""
-vim.opt.formatoptions:remove({'r', 'o'})
+-- disable mouse
+vim.opt.mouse = ""
 
--- Who needs airline?
+-- disable auto-comment
+vim.opt.formatoptions:remove({'c', 'r', 'o'}) 
+
+-- who needs airline?
 function status()
   return ' %f %m%r%y '
   .. (vim.opt.spell:get() and '[Spell ('
@@ -84,24 +77,26 @@ function status()
   .. ')]' or '')
   .. '%=(%l, %c) (0x%B) (%P) [%L] '
 end
-
 vim.opt.statusline = '%!v:lua.status()'
 
-vim.opt.writebackup = false -- Don't make backup
-vim.opt.backup      = false -- Don't keep backup
+-- don't keep or make backup
+vim.opt.writebackup = false
+vim.opt.backup      = false
 
--- Indentation
+-- indentation
 vim.opt.smartindent = true
-vim.opt.expandtab   = false -- Don't expand tabs
-vim.opt.shiftwidth  = 0     -- Default to tabstop
+vim.opt.expandtab = false  -- don't expand tabs by default
+vim.opt.shiftwidth = 0     -- default to tabstop
 
+-- leader is space
 vim.g.mapleader = ' '
 
+-- don't know why the default is cpp
 vim.g.c_syntax_for_h = true
 
-vim.g.gruvbox_contrast_dark = 'hard'
-vim.cmd.colorscheme('gruvbox')
+vim.cmd.colorscheme('moonfly')
 
+-- custom indentation per filetype
 local typecmd = {
   html = [[ setlocal ts=2 ]],
   tex  = [[ setlocal ts=2 ]],
@@ -110,18 +105,20 @@ local typecmd = {
   vim  = [[ setlocal ts=2 ]],
   sh   = [[ setlocal ts=2 ]],
   asm  = [[ setlocal ts=2 ]],
+  go   = [[ setlocal ts=4 ]],
   cpp  = [[ setlocal ts=4 ]],
   lua  = [[ setlocal ts=2 et ]],
   elm  = [[ setlocal ts=2 et ]],
   javascript = [[ setlocal ts=4 et ]],
   markdown = [[ setlocal ts=4 et ]],
+  haskell = [[ setlocal ts=4 et ]],
 }
 
 for filetype, cmd in pairs(typecmd) do
   autocmd("FileType", { pattern = { filetype }, command = cmd })
 end
 
--- Auto-save
+-- auto-save
 autocmd({"TextChanged", "InsertLeave"}, {
   pattern = { '*' },
   callback = function()
@@ -131,19 +128,22 @@ autocmd({"TextChanged", "InsertLeave"}, {
   end,
 })
 
+-- toggle search
+vim.keymap.set('n', '<F3>', function()
+  fn.setreg('/', (fn.getreg('/') == '') and fn.expand("<cword>") or '')
+end)
+
+-- telescope config
 local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
 vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
 vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
 vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
 
-vim.keymap.set('n', '<F3>', function()
-  fn.setreg('/', (fn.getreg('/') == '') and fn.expand("<cword>") or '')
-end)
-
-
+-- treesitter config
 require('nvim-treesitter.configs').setup {
-  ensure_installed = { "c", "cpp", "lua", "vim", "python", "go" },
+  -- install others as you go
+  ensure_installed = { "c", "cpp", "lua", "vim", "python" },
   sync_install = false,
   auto_install = false,
   highlight = {
